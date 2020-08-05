4b240d2336fff6d12f296822dcf3100d5e3ff1dc
margo10011990 <margo10011990@gmail.com>
2020-08-05 15:42:07 +0300

    Updates

---
+++ precache-manifest.7be67b547de8d8542b322677eade48a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98535894007e5fa45bc7d0bd1a8a947f",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "15edc3107eb63eb59dd2",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.a364a8a2.chunk.css"
+  },
+  {
+    "revision": "7c435b9bdc9c47fca7bc",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.ca6955eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.ca6955eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15edc3107eb63eb59dd2",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.989bafb3.chunk.js"
+  },
+  {
+    "revision": "2a13091ca041de263bee",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.650e6e2b.js"
+  }
+]);
